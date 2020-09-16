a99dde14d03e193b0b7949c9e6fb101ce3df8c93
Moses Ekpai <ekpaimoise03@gmail.com>
2020-09-15 19:59:21 -0500

    first commit

---
+++ projectreact/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import Tweet from './Tweet'
+
+function App(){
+  const[isRed,setRed]=useState(false);
+  const [count, setCount]= useState(0);
+  const increment = ()=>{
+    setCount(count +1);
+  };
+
+
+  return(
+    <div className= 'app'> 
+    <button onClick={increment}> Iconcrement</button>
+    <h1>{count}</h1>
+    </div>
+  );
+}
+export default App;
